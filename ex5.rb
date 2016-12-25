93fa84dc99a1c5e8b46cb1776129c5cc056e33c8
Martin Ansong <martin.ansong@gmail.com>
2016-12-25 21:33:44 +0000

    more variables and printing

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+my_name = 'Martin K. Ansong'
+my_age = 35
+my_height = 5.8 # 5 feet 8 inches
+my_weight = 91.7 # kg; mostly muscle
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} kilograms heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
+

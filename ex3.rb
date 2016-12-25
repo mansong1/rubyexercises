43a5e3481b9d071f7a762cd5629cf2b7b5bc5b86
Martin Ansong <martin.ansong@gmail.com>
2016-12-25 20:49:39 +0000

    rewriting to use floating point numbers

--- ex3.rb
+++ ex3.rb
@@ -2,14 +2,14 @@
 puts 'I will now count my chickens:'
 
 # prints string and computed values
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # prints string
 puts 'Now I will count the eggs:'
 
 # prints computed value
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # prints string; no computation is carried out as there is no #{}
 puts 'Is it true that 3 + 2 < 5 - 7?'
@@ -18,16 +18,16 @@ puts 'Is it true that 3 + 2 < 5 - 7?'
 puts 3 + 2 < 5 - 7
 
 # prints string and computed values
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
 
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 puts "Oh, that's why it's false."
 
 puts 'How about some more.'
 
-puts "Is is greater? #{5 > -2}"
+puts "Is is greater? #{5.0 > -2.0}"
 
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
 
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2.0}"

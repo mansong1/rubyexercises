ccd902bd85d93a556488e4b377300023c95ccfef
Martin Ansong <martin.ansong@gmail.com>
2016-12-25 21:41:14 +0000

    remove my_ and add cm and pound conversion vars

--- ex5.rb
+++ ex5.rb
@@ -1,15 +1,18 @@
-my_name = 'Martin K. Ansong'
-my_age = 35
-my_height = 5.8 # 5 feet 8 inches
-my_weight = 91.7 # kg; mostly muscle
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'Martin K. Ansong'
+age = 35
+height = 5.8 # 5 feet 8 inches
+weight = 91.7 # kg; mostly muscle
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+height_cm = height * 2.54
+weight_pounds = weight * 2.20
 
-puts "Let's talk about #{my_name}"
-puts "He's #{my_height} kilograms heavy"
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "Let's talk about #{name}"
+puts "He's #{height} inches tall which is #{height_cm} in centimeters"
+puts "He's #{weight} kilograms heavy and #{weight_pounds} in pounds"
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
 

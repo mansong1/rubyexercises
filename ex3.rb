e53c9d34b3e16684a93b84c4bed6f6706903e98b
Martin Ansong <martin.ansong@gmail.com>
2016-12-25 20:46:41 +0000

    Numbers and Maths

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+# prints string
+puts 'I will now count my chickens:'
+
+# prints string and computed values
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# prints string
+puts 'Now I will count the eggs:'
+
+# prints computed value
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints string; no computation is carried out as there is no #{}
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+# prints boolean false
+puts 3 + 2 < 5 - 7
+
+# prints string and computed values
+puts "What is 3 + 2? #{3 + 2}"
+
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts 'How about some more.'
+
+puts "Is is greater? #{5 > -2}"
+
+puts "Is it greater or equal? #{5 >= -2}"
+
+puts "Is it less or equal? #{5 <= -2}"

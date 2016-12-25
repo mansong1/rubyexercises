b2a79372c179cf15f38b16525b8c7f184431295b
Martin Ansong <martin.ansong@gmail.com>
2016-12-25 20:10:01 +0000

    comments and octothorpe

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so we can read the program later
+# Anything after # is ignored
+
+puts "I could have code like this" # and the comment after is ignored
+
+# Comments can also "disable" or comment out a piece of code
+# puts "This won't run"
+
+puts "This will run"

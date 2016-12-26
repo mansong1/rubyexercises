7cc6a7a20faef72f73f50f6731ea23c9a42c5d93
Martin Ansong <martin.ansong@gmail.com>
2016-12-26 23:00:28 +0000

    reading files

---
+++ ex15.rb
@@ -0,0 +1,15 @@
+# read cli input as filename
+filename = ARGV.first
+
+# open the file
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+print txt.read # print content of file
+
+print 'Type the filename again: '
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read

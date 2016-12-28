6ce72e20bff8950eded0043a8a942d7bb6a445f3
Martin Ansong <martin.ansong@gmail.com>
2016-12-28 22:40:53 +0000

    functions and variables

---
+++ ex19.rb
@@ -0,0 +1,28 @@
+# function to take values for cheese count and boxes
+# of crackers
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket.\n"
+end
+
+puts 'We can just give the function numbers directly:'
+# call function
+cheese_and_crackers(20,30)
+
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+# call function with variables
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+puts 'We can even do maths inside too:'
+cheese_and_crackers(10 + 20, 5 + 6)
+
+puts 'And we can combine the two, variables and math:'
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 20)
+
+

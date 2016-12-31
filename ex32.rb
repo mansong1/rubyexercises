a124e2e7f65137d5ea0dfbce63229f0b96912176
Martin Ansong <martin.ansong@gmail.com>
2016-12-31 21:00:52 +0000

    loops and arrays

---
+++ ex32.rb
@@ -0,0 +1,25 @@
+the_count = [1,2,3,4,5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+for number in the_count
+  puts "This is count #{number}"
+end
+
+# same as above different syntax
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# same as above different syntax
+change.each {|i| puts "I got #{i}" }
+
+elements = []
+
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  # push the i variable to the *end* of the list
+  elements.push(i)
+end
+
+elements.each {|i| puts "Element was: #{i}"}

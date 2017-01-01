bc8b916797ddd29c7d53e2295ffe837a50bcce22
Martin Ansong <martin.ansong@gmail.com>
2017-01-01 17:42:28 +0000

    Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1 the 2nd animal and is a ruby
+puts animals[1]
+# 2. The third (3rd) animal is at 2 and is a peacock
+puts animals[2]
+# 3. The first (1st) animal is at 0 and is a bear
+puts animals[0]
+# 4. The animal at 3 is the 4th animal and is a kangaroo
+puts animals[3]
+# 5. The fifth (5th) animal is at 4 and is a whale
+puts animals[4]
+# 6. The animal at 2 is the 3rd animal and is a peacock
+puts animals[2]
+# 7. The sixth (6th) animal is the animal at 5 and is a platypus
+puts animals[5]
+# 8. The animal at 4 is the 5th animal and is a whale
+puts animals[4]

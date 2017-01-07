30ff5b0d22289d8d0f2db284bc3cbdfc68f6c146
Martin Ansong <martin.ansong@gmail.com>
2017-01-07 23:01:02 +0000

    complete exercise 40

--- ex40.rb
+++ ex40.rb
@@ -1,7 +1,28 @@
-class song
+class Song
 
-  def initialise(lyrics)
+  def initialize(lyrics)
     @lyrics = lyrics
   end
 
-  
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line}
+  end
+end
+
+happy_bday = Song.new(["Happy birthday to you",
+           "I don't want to get sued",
+           "So I'll stop right there"])
+
+bulls_on_parade = Song.new(["They rally around tha family",
+            "With pockets full of shells"])
+
+wheelsonthebus = ["The wheels on the bus go round and round,
+  round and round."]
+
+wheel_on_the_bus = Song.new(wheelsonthebus)
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+wheel_on_the_bus.sing_me_a_song()
